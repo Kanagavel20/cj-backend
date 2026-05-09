6778223b42b4395a4bd8829fec4b076122de84e2
Kanagavel <balusathish185@gmail.com>
2026-05-09 10:44:05 +0530

    cors

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ dataBase();
 app.use(express.json())
 app.use(cors())
 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/',indexRoutes)
 

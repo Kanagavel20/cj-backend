c25ac4173824d5e34d42fd48f79c47e82e366035
Kanagavel <balusathish185@gmail.com>
2026-05-23 00:05:15 +0530

    db

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
         // await mongoose.connect("mongodb://127.0.0.1:27017/cracker_junction");
-        await mongoose.connect("mongodb+srv://balusathish185_db_user:SR2b8jk2xMflNicJ@crackerjunction.z3olodk.mongodb.net/");
+        await mongoose.connect("mongodb+srv://balusathish185_db_user:SR2b8jk2xMflNicJ@crackerjunction.z3olodk.mongodb.net/test");
         console.log("MongoDB connected");
     } catch (error) {
         console.error("MongoDB connection error:", error.message);
